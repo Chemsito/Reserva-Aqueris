be2281be9228753df8d108d4e4bce8c5fb0d60a3
Aaron Diaz <90489938+Chemsito@users.noreply.github.com>
2026-08-24 15:01:46 -0500

    feat: configure Supabase client

---
+++ config.js
@@ -0,0 +1,5 @@
+window.AQUERIS_CONFIG = Object.freeze({
+  supabaseUrl: "https://eiqwcakqobqocotbgucw.supabase.co",
+  supabaseKey: "sb_publishable_K21KAZuzHVMJQROF1CCoAw_8AcMWDM1",
+  storageBucket: "product-images"
+});
